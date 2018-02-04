1aca3601a8112df8ba3cca7c210e518a9543ba2e
Zhenzhong Mao <dkrose.random@gmail.com>
2018-02-04 20:22:26 +0800

    commit omg

---
+++ classof.js
@@ -0,0 +1,10 @@
+function classof(o){
+    if(o===null) return "Null";
+    if(o===undefined) return "Undefined";
+    return Object.prototype.toString.call(o).slice(8,-1);
+}
+
+console.log(classof(1));
+console.log(classof({}));
+console.log(classof(false));
+console.log(classof('false'));
